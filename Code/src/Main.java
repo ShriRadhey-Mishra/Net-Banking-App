25425fbf0bbfa32214e5184fa20664d8c924b970
ShriRadhey-Mishra <radheymishra13@gmail.com>
2025-07-24 00:48:54 +0530

    Great start! Planning what is needed

---
+++ Code/src/Main.java
@@ -0,0 +1,22 @@
+/* Sign Up
+ * Name
+ * email
+ * phone number
+ * set up pin
+ *
+ * */
+
+/* Sign In */
+
+/* Account Check Up
+ * Balance Check
+ * Transaction check
+ *
+ * */
+
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
