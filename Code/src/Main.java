4c5e2a68595b660d5dd11a95cb0b50806d0c51cf
ShriRadhey-Mishra <radheymishra13@gmail.com>
2025-07-25 01:11:17 +0530

    basic structure

--- Code/src/Main.java
+++ Code/src/Main.java
@@ -1,21 +1,4 @@
 /**
- * Functionality - Sign Up
- * Name
- * email
- * phone number
- * set up PIN
- * save
- */
-
-/**
- * Functionality - Sign In
- * username or email
- * password
- * check
- * */
-
-
-/**
  * Functionality - Actions
  * Make Transaction
  * take loan
@@ -31,9 +14,53 @@
  * Loans
  * */
 
+import java.util.Scanner;
 
 public class Main {
+    public static void SignUp() {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = scan.nextLine();
+
+//        Validate this email
+        System.out.print("Enter your email: ");
+        String email = scan.nextLine();
+
+        System.out.print("Create a Password: ");
+        String password = scan.nextLine();
+
+        System.out.print("Enter your phone number: ");
+        long phoneNumber = scan.nextLong();
+
+//        Validate the pin if longer than 4
+        System.out.print("Set a 4 digit PIN for your account: ");
+        int pin = scan.nextInt();
+
+    }
+    public static void SignIn() {
+        Scanner scan = new Scanner(System.in);
+
+//        Check if username or password exists
+        System.out.print("Username (or Email): ");
+        String userNameOrEmail = scan.nextLine();
+
+//        Check if this password is associated with this username or email
+        System.out.print("Password: ");
+        String password = scan.nextLine();
+
+    }
+
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Choose one option: \n1. Sign Up \n2. Sign In \n3. Exit \n(Pick one number): ");
+        int userInput = scan.nextInt();
+        System.out.print('\n');
+
+        if (userInput == 1) SignUp();
+        else if (userInput == 2) SignIn();
+        else System.out.println("See you soon!");
 
     }
 }
