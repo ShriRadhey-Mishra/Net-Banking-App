5d9b30dd73e8b99c898409253d91b34cb43395cb
ShriRadhey-Mishra <radheymishra13@gmail.com>
2025-07-26 01:58:07 +0530

    Sign Up method created

--- Code/src/Main.java
+++ Code/src/Main.java
@@ -15,9 +15,20 @@
  * */
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
-    public static void SignUp() {
+//    email should contain a-z A-Z 0-9 . @ .com
+    public static boolean validateEMail(String email) {
+        String regex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(email);
+
+        return matcher.matches();
+    }
+    public static User SignUp() {
         Scanner scan = new Scanner(System.in);
 
         System.out.print("Enter your name: ");
@@ -25,20 +36,35 @@ public class Main {
 
 //        Validate this email
         System.out.print("Enter your email: ");
-        String email = scan.nextLine();
+        String email = scan.nextLine().trim().toLowerCase();
+        while (!validateEMail(email)) {
+            System.out.print("Enter a valid email: ");
+            email = scan.nextLine();
+        }
 
         System.out.print("Create a Password: ");
         String password = scan.nextLine();
 
         System.out.print("Enter your phone number: ");
         long phoneNumber = scan.nextLong();
+        while (Long.toString(phoneNumber).length() != 10) {
+            System.out.print("Enter a valid phone number: ");
+            phoneNumber = scan.nextLong();
+        }
 
 //        Validate the pin if longer than 4
         System.out.print("Set a 4 digit PIN for your account: ");
-        int pin = scan.nextInt();
+        String pin = scan.nextLine();
+        while (pin.length() != 4) {
+            System.out.print("Set a valid 4 digit PIN for your account: ");
+            pin = scan.nextLine();
+        }
 
+//        Save all this data in JDBC Database
+
+        return new User(name, email, password, phoneNumber, pin);
     }
-    public static void SignIn() {
+    public static User SignIn() {
         Scanner scan = new Scanner(System.in);
 
 //        Check if username or password exists
@@ -49,17 +75,19 @@ public class Main {
         System.out.print("Password: ");
         String password = scan.nextLine();
 
+//        retrieve the other detail from the database
     }
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        User user;
 
         System.out.print("Choose one option: \n1. Sign Up \n2. Sign In \n3. Exit \n(Pick one number): ");
         int userInput = scan.nextInt();
         System.out.print('\n');
 
-        if (userInput == 1) SignUp();
-        else if (userInput == 2) SignIn();
+        if (userInput == 1) user = SignUp();
+        else if (userInput == 2) user = SignIn();
         else System.out.println("See you soon!");
 
     }
---
+++ Code/src/User.java
@@ -0,0 +1,19 @@
+public class User {
+    public String name, email, password;
+    public long phoneNumber;
+    public String pin;
+
+    public User(String name, String email, String password, long phoneNumber, String pin) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+        this.pin = pin;
+    }
+
+    public User(String name, String password, String email) {
+        this.name = name;
+        this.password = password;
+        this.email = email;
+    }
+}
