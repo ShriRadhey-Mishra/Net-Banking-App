c3f8cf9b3f490e0e6886df596923b7e9f2a4e792
ShriRadhey-Mishra <radheymishra13@gmail.com>
2025-08-30 03:50:22 +0530

    added sub-folders, which will help handling data for multiple users.

--- Code/src/Main.java
+++ Code/src/Main.java
@@ -5,14 +5,16 @@
  * pay loan
  */
 
-/** Functionality - Management
+/**
+ * Functionality - Management
  * Manage App
  * take PIN to check all these
  * Account Check Up
  * Balance Check
  * Transaction check
  * Loans
- * */
+ *
+ */
 
 import java.io.*;
 import java.util.ArrayList;
@@ -29,7 +31,7 @@ public class Main {
         for (int i = 0; i < 50; ++i) System.out.println();
     }
 
-    //    Email should contain a-z A-Z 0-9 . @ .com - Done
+    //    Email should contain aA-zZ 0-9 . @ .com - Done
     public static boolean invalidateEMail(String email) {
         String regex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$";
 
@@ -71,17 +73,20 @@ public class Main {
             pin = scan.nextLine();
         }
         // Saves all this data in File
-        String fileName = name.toLowerCase().replace(" ", "").replace(".", "") + "@" + email.replace(".", "").split("@")[0] + ".txt";
+        String subFolderName = name.toLowerCase().replace(" ", "").replace(".", "") + "@" + email.replace(".", "").split("@")[0];
         try {
-            File userData = new File("UserData/", fileName);
-            if (!userData.exists()) {
-                FileWriter enterUserData = new FileWriter(userData
-                );
-                enterUserData.write(name + "\n" + email + "\n" + password + "\n" + phoneNumber + "\n" + pin);
-                enterUserData.close();
-            } else {
-                System.out.println("A user already exists with this Username and Email.");
-            }
+            File mainFolder = new File("UserData/");
+            if (!mainFolder.exists()) mainFolder.mkdir();
+            File subFolder = new File(mainFolder, subFolderName);
+            if (!subFolder.exists()) {
+                subFolder.mkdir();
+                File userData = new File(subFolder, "credentials.txt");
+                if (!userData.exists()) {
+                    FileWriter enterUserData = new FileWriter(userData);
+                    enterUserData.write(name + "\n" + email + "\n" + password + "\n" + phoneNumber + "\n" + pin);
+                    enterUserData.close();
+                }
+            } else System.out.println("A User already exists with this username/ email.");
         } catch (IOException e) {
             System.out.println("An error occurred while processing the user data.\n" + e.getMessage());
         }
@@ -99,8 +104,8 @@ public class Main {
         System.out.print("Enter your email: ");
         String email = scan.nextLine().trim().toLowerCase();
         while (invalidateEMail(email)) {
-            System.out.print("Enter your email: ");
             System.out.println("Invalid email.");
+            System.out.print("Enter your email: ");
             email = scan.nextLine().trim().toLowerCase();
         }
 
@@ -109,43 +114,44 @@ public class Main {
         String password = scan.nextLine();
 
         //  retrieve the other detail from the database
-        File directory = new File("UserData");
-        String searchTerm = email.replace(".", "").split("@")[0] + ".txt";
-        String file = "";
-        File[] matchingFiles = directory.listFiles(new FilenameFilter() {
-            public boolean accept(File dir, String name) {
-                return name.contains(searchTerm);
-            }
-        });
-        // uses email to find file in the Local file system
-        if (matchingFiles != null) {
-            for (File files : matchingFiles) {
-                file = files.getName();
-            }
-            try {
-                // when file is found it collects userdata
-                List<String> data = new ArrayList<>();
-                File userData = new File("UserData", file);
-                Scanner scanner = new Scanner(userData);
-                while (scanner.hasNextLine()) {
-                    data.add(scanner.nextLine());
-                }
-                scanner.close();
-                // if password matches with the one in the file it returns user else it returns null
-                if (!Objects.equals(data.get(2), password)) {
-                    System.out.println("User not found: UserData (Access Denied)");
-                    return null;
-                } else {
-                    System.out.println("Log In Successful!");
-                    return new User(data.get(0), data.get(1), data.get(2), Long.parseLong(data.get(3)), data.get(4));
-                }
+        File mainFolder = new File("UserData");
+        String subFolderName = email.replace(".", "").split("@")[0];
+        File userFolder = new File(mainFolder, subFolderName);
+        if (userFolder.exists() && userFolder.isDirectory()) {
+           File credentials = new File(userFolder, "credentials.txt");
+           if (credentials.exists()) {
+               try {
+                   List<String> userData = new ArrayList<>();
+                   Scanner scanner = new Scanner(credentials);
+                   while (scanner.hasNextLine()) {
+                       userData.add(scanner.nextLine());
+                   }
+                   scanner.close();
 
-            } catch (FileNotFoundException e) {
-                System.out.println("User not found: " + e.getMessage());
-            }
+                   if (!Objects.equals(password, userData.get(2))) {
+                       System.out.println("Wrong Password");
+                       return null;
+                   } else {
+                       System.out.println("Log In Successful!");
+                       return new User(
+                               userData.get(0),                  // name
+                               userData.get(1),                  // email
+                               userData.get(2),                  // password
+                               Long.parseLong(userData.get(3)),  // phone
+                               userData.get(4)                   // pin
+                       );
+                   }
+
+               } catch (FileNotFoundException e) {
+                   System.out.println("User not found: " + e.getMessage());
+               }
+           } else {
+               System.out.println("User credentials not found.");
+           }
         } else {
-            System.out.println("Data not found!");
+            System.out.println("User does not exists with username/ email.");
         }
+
         return null;
     }
 
@@ -171,12 +177,10 @@ public class Main {
                     System.out.println("An error occurred while processing the user data.\n" + e.getMessage());
 
                 }
-            }
-            else if (userInput == 2) {
+            } else if (userInput == 2) {
                 user = SignIn();
 
-            }
-            else {
+            } else {
                 System.out.println("See you soon!");
                 Thread.sleep(3000);
                 user = null;
